f23e4da5707b73756957ad3dc4fb954f84ebb0df
Alishah Solanki <alishahsolanki@retrocubes-MacBook-Pro.local>
2019-12-11 11:33:28 +0500

    font scaling fix

--- src/theme/Fonts.js
+++ src/theme/Fonts.js
@@ -8,47 +8,47 @@
 import Metrics from "./Metrics";
 
 export default class Fonts {
-    static FontFamily = {
-        default: "Gibson",
-        secondary: "Georgia"
-    };
+	static FontFamily = {
+		default: "Gibson",
+		secondary: "Georgia"
+	};
 
-    static Type = {
-        BoldItalic: "BoldItalic",
-        Regular: "Regular",
-        SemiboldIt: "SemiboldIt",
-        Italic: "Italic",
-        Bold: "Bold",
-        LightIt: "LightIt",
-        SemiBold: "SemiBold",
-        Light: "Light"
-    };
+	static Type = {
+		BoldItalic: "BoldItalic",
+		Regular: "Regular",
+		SemiboldIt: "SemiboldIt",
+		Italic: "Italic",
+		Bold: "Bold",
+		LightIt: "LightIt",
+		SemiBold: "SemiBold",
+		Light: "Light"
+	};
 
-    static Size = {
-        xxxSmall: Metrics.generatedFontSize(11),
-        xxSmall: Metrics.generatedFontSize(13),
-        xSmall: Metrics.generatedFontSize(14),
-        small: Metrics.generatedFontSize(15),
-        normal: Metrics.generatedFontSize(17),
-        medium: Metrics.generatedFontSize(19),
-        large: Metrics.generatedFontSize(21),
-        xLarge: Metrics.generatedFontSize(23),
-        xxLarge: Metrics.generatedFontSize(28),
-        xxxLarge: Metrics.generatedFontSize(31),
-        huge: Metrics.generatedFontSize(34),
-        xhuge: Metrics.generatedFontSize(37),
-        xxhuge: Metrics.generatedFontSize(40),
-        xxxhuge: Metrics.generatedFontSize(43)
-    };
+	static Size = {
+		xxxSmall: 11,
+		xxSmall: 13,
+		xSmall: 14,
+		small: 15,
+		normal: 17,
+		medium: 19,
+		large: 21,
+		xLarge: 23,
+		xxLarge: 28,
+		xxxLarge: 31,
+		huge: 34,
+		xhuge: 37,
+		xxhuge: 40,
+		xxxhuge: 43
+	};
 
-    static font = (
-        fontFamily = Fonts.FontFamily.default,
-        type = Fonts.Type.Regular,
-        size = Fonts.Size.medium
-    ) => {
-        return {
-            fontFamily: fontFamily + "-" + type,
-            fontSize: size
-        };
-    };
+	static font = (
+		fontFamily = Fonts.FontFamily.default,
+		type = Fonts.Type.Regular,
+		size = Fonts.Size.medium
+	) => {
+		return {
+			fontFamily: fontFamily + "-" + type,
+			fontSize: size
+		};
+	};
 }
